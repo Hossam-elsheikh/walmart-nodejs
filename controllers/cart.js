c622b9c4d22f8934c82bb87433a7af7930c5a0f6
SaraWilliam789 <sarawillim1993@gmail.com>
2023-09-24 09:24:27 +0300

    first

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,4 +1,5 @@
 let customerModel = require('../models/Customers');
+
 const jwt =require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
@@ -34,6 +35,23 @@ let deleteProduct = async (req,res)=>{
         res.status(500).json({message:err.message})
     }
 }
+let deletecart = async (req,res)=>{
+    let id = req.id
+    let role = req.role
+    let product_id= req.params.id
+    if(role !== 'user'){
+        return res.status(401).json({message:"login as a user"})
+    }
+    try{
+        let customer = await customerModel.findOne({_id:id})
+        let newCart = customer.cart.filter((p)=>p._id == product_id)
+        console.log(newCart);
+        let updatedCustomer = await customerModel.updateOne({_id:id},{$set:{'cart':newCart}})
+        res.status(200).json({message:"cart successfully deleted",cart : updatedCustomer})
+    }catch(err){
+        res.status(500).json({message:err.message})
+    }
+}
 
 
-module.exports = {getCart,deleteProduct}
+module.exports = {getCart,deleteProduct,deletecart}
--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,14 @@
 const express = require('express');
 let {auth} = require('../middlewares/auth')
-let {getCart,deleteProduct} = require('../controllers/cart')
+let {getCart,deleteProduct,deletecart} = require('../controllers/cart')
 
 const Router = express.Router();
 
 Router.get('/',auth,getCart)
 
-Router.delete('/:id',auth,deleteProduct)
+Router.patch('/:id',auth,deleteProduct)
+
+Router.delete('/:id',auth,deletecart)
+
 
 module.exports = Router;
