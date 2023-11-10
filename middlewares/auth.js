98da3057f9cf8f0870ba0ff5fd7f5dd2f8066580
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-11-10 17:36:14 +0200

    s

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const { promisify } = require('util');
 async function auth(req, res, next) {
-    let {authorization} = req.headers
+    let authorization = req.body.auth || req.headers.authorization
+    console.log(authorization);
     if (!authorization) {
         return res.status(401).json({message:"Please Login First dude!"})
     }
