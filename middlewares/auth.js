39b7a748bedb7e4a50a670a8f2daca5c4a8a5503
FCICoder <mahmoud.aadel1998@gmail.com>
2023-11-11 16:18:33 +0200

    'fixx'

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const { promisify } = require('util');
 async function auth(req, res, next) {
-    let authorization = req.body.auth || req.headers.authorization
-    console.log(authorization);
+    let authorization = req.body.auth || req.headers.authorization || req.body.headers.authorization
     if (!authorization) {
         return res.status(401).json({message:"Please Login First dude!"})
     }
